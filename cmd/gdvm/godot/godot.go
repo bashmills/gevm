2d378b6ec0657d73fe883ef7b7d7ccbea6a9518c
Bash Mills <bashmills@gmail.com>
2025-01-30 22:31:18 +0000

    Add use command

--- cmd/gdvm/godot/godot.go
+++ cmd/gdvm/godot/godot.go
@@ -106,6 +106,21 @@ func (c *Install) Run(app *gdvm.App) error {
 	return nil
 }
 
+type Use struct {
+	Version string `arg:"" help:"Godot version to use in the format x.x.x.x, x.x.x or x.x"`
+	Release string `default:"stable" help:"Release to use (dev1, alpha2, beta3, rc4, stable, etc)"`
+	Mono    bool   `help:"Use mono version"`
+}
+
+func (c *Use) Run(app *gdvm.App) error {
+	err := app.Godot.Use(semver.Maybe(c.Version, c.Release, c.Mono))
+	if err != nil {
+		return fmt.Errorf("cannot use godot: %w", err)
+	}
+
+	return nil
+}
+
 type List struct{}
 
 func (c *List) Run(app *gdvm.App) error {
@@ -121,5 +136,6 @@ type Godot struct {
 	Download  Download  `cmd:"" help:"Download godot engine to the cache by version"`
 	Uninstall Uninstall `cmd:"" help:"Uninstall godot engine by version"`
 	Install   Install   `cmd:"" help:"Install godot engine by version"`
+	Use       Use       `cmd:"" help:"Use godot engine by version"`
 	List      List      `cmd:"" help:"List all current godot versions"`
 }
