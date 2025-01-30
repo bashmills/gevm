bacf5408a4a8a2df2f7dd2774353d15898764b9f
Bash Mills <bashmills@gmail.com>
2025-01-30 22:31:17 +0000

    Godot command flags for including build templates in operations

--- cmd/gdvm/godot/godot.go
+++ cmd/gdvm/godot/godot.go
@@ -8,12 +8,20 @@ import (
 )
 
 type Download struct {
-	Version string `arg:"" help:"Godot version to download to cache in the format x.x.x.x, x.x.x or x.x"`
-	Release string `default:"stable" help:"Release to use (dev1, alpha2, beta3, rc4, stable, etc)"`
-	Mono    bool   `help:"Use mono version"`
+	Version               string `arg:"" help:"Godot version to download to cache in the format x.x.x.x, x.x.x or x.x"`
+	IncludeBuildTemplates bool   `help:"Include build templates in download"`
+	Release               string `default:"stable" help:"Release to use (dev1, alpha2, beta3, rc4, stable, etc)"`
+	Mono                  bool   `help:"Use mono version"`
 }
 
 func (c *Download) Run(app *gdvm.App) error {
+	if c.IncludeBuildTemplates {
+		err := app.BuildTemplates.Download(semver.Maybe(c.Version, c.Release, c.Mono))
+		if err != nil {
+			return fmt.Errorf("cannot download build templates: %w", err)
+		}
+	}
+
 	err := app.Godot.Download(semver.Maybe(c.Version, c.Release, c.Mono))
 	if err != nil {
 		return fmt.Errorf("cannot download godot: %w", err)
@@ -23,12 +31,20 @@ func (c *Download) Run(app *gdvm.App) error {
 }
 
 type Uninstall struct {
-	Version string `arg:"" help:"Godot version to uninstall in the format x.x.x.x, x.x.x or x.x"`
-	Release string `default:"stable" help:"Release to use (dev1, alpha2, beta3, rc4, stable, etc)"`
-	Mono    bool   `help:"Use mono version"`
+	Version               string `arg:"" help:"Godot version to uninstall in the format x.x.x.x, x.x.x or x.x"`
+	ExcludeBuildTemplates bool   `help:"Exclude build templates in uninstall"`
+	Release               string `default:"stable" help:"Release to use (dev1, alpha2, beta3, rc4, stable, etc)"`
+	Mono                  bool   `help:"Use mono version"`
 }
 
 func (c *Uninstall) Run(app *gdvm.App) error {
+	if !c.ExcludeBuildTemplates {
+		err := app.BuildTemplates.Uninstall(semver.Maybe(c.Version, c.Release, c.Mono), true)
+		if err != nil {
+			return fmt.Errorf("cannot uninstall build templates: %w", err)
+		}
+	}
+
 	err := app.Godot.Uninstall(semver.Maybe(c.Version, c.Release, c.Mono), true)
 	if err != nil {
 		return fmt.Errorf("cannot uninstall godot: %w", err)
@@ -38,12 +54,20 @@ func (c *Uninstall) Run(app *gdvm.App) error {
 }
 
 type Install struct {
-	Version string `arg:"" help:"Godot version to download and install in the format x.x.x.x, x.x.x or x.x"`
-	Release string `default:"stable" help:"Release to use (dev1, alpha2, beta3, rc4, stable, etc)"`
-	Mono    bool   `help:"Use mono version"`
+	Version               string `arg:"" help:"Godot version to download and install in the format x.x.x.x, x.x.x or x.x"`
+	IncludeBuildTemplates bool   `help:"Include build templates in install"`
+	Release               string `default:"stable" help:"Release to use (dev1, alpha2, beta3, rc4, stable, etc)"`
+	Mono                  bool   `help:"Use mono version"`
 }
 
 func (c *Install) Run(app *gdvm.App) error {
+	if c.IncludeBuildTemplates {
+		err := app.BuildTemplates.Install(semver.Maybe(c.Version, c.Release, c.Mono))
+		if err != nil {
+			return fmt.Errorf("cannot install build templates: %w", err)
+		}
+	}
+
 	err := app.Godot.Install(semver.Maybe(c.Version, c.Release, c.Mono))
 	if err != nil {
 		return fmt.Errorf("cannot install godot: %w", err)
